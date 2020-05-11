31af3b77790fb7e9218bd200fbc15ec16615710c
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-05-11 23:49:02 +0100

    Fix logic bug in authorization

--- src/apps/express-backend/src/app/middleware/authorize.middleware.ts
+++ src/apps/express-backend/src/app/middleware/authorize.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 export const authorize = (...roles:string[])=>{
     return (req:Request,res:Response,next:NextFunction)=>{
-        if(!(req as any).user && roles.indexOf((req as any).user.role)<0){
+        if(!(req as any).user || roles.indexOf((req as any).user.role)<0){
             res.status(403);
             res.json({
                 error:{
