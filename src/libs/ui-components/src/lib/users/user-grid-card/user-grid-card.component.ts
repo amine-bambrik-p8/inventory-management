6af2ff26995711b581f96ea01a928b46d1c12590
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-06-28 18:39:48 +0100

    Update User Grid Card to have actions for delete & edit

--- src/libs/ui-components/src/lib/users/user-grid-card/user-grid-card.component.ts
+++ src/libs/ui-components/src/lib/users/user-grid-card/user-grid-card.component.ts
@@ -1,5 +1,7 @@
+import { Router } from '@angular/router';
 import { IUser } from '@workspace/interfaces';
 import { Component, OnInit, Input } from '@angular/core';
+import { UsersFacade } from '@workspace/core-data';
 
 @Component({
   selector: 'workspace-user-grid-card',
@@ -9,9 +11,20 @@ import { Component, OnInit, Input } from '@angular/core';
 export class UserGridCardComponent implements OnInit {
   @Input()
   user:IUser;
-  constructor() { }
+  constructor(private router:Router,private usersFacade:UsersFacade) { }
 
   ngOnInit(): void {
+  }
+
+  onEdit(user: IUser){
+    this.router.navigate(["users",user._id,"edit"]);
+  }
+  onDelete(user: IUser){
+    try {
+      this.usersFacade.deleteUser(user);
+    } catch (error) {
+      console.error(error);      
+    }
   }
 
 }
