baa93b5f93c0461424c63314a9cb830448dfb353
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-06-28 19:47:41 +0100

    Add Product entries reducer

--- src/libs/core-data/src/lib/state/products/products.reducer.ts
+++ src/libs/core-data/src/lib/state/products/products.reducer.ts
@@ -2,8 +2,7 @@ import { ProductsActions, ProductsActionTypes } from './products.actions';
 import { IProduct } from '@workspace/interfaces';
 import { EntityAdapter,EntityState,createEntityAdapter } from '@ngrx/entity';
 import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { selectAllSuppliers } from '../suppliers/suppliers.reducer';
-import { selectAllCategories } from '../categories/categories.reducer';
+import { ProductEntriesActionTypes, ProductEntriesActions } from './product-entries/product-entries.actions';
 
 export interface ProductsState extends EntityState<IProduct>{
     selectedProduct?:IProduct;
@@ -21,7 +20,7 @@ export function productsReducers(state = initialState,action:ProductsActions): P
         case ProductsActionTypes.PRODUCT_READ:
             return {...state,selectedProduct:action.payload};
         case ProductsActionTypes.PRODUCTS_LOADED:
-            return adapter.setAll(action.payload,state);
+            return adapter.upsertMany(action.payload,state);
         case ProductsActionTypes.PRODUCT_CREATED:
             return adapter.addOne(action.payload,state);
         case ProductsActionTypes.PRODUCT_DELETED:
@@ -32,6 +31,26 @@ export function productsReducers(state = initialState,action:ProductsActions): P
                 id,
                 changes,
             },state);
+        default:
+            return productEntriesReducers(state,action as any);
+    }
+}
+
+
+export function productEntriesReducers(state:ProductsState = initialState,action:ProductEntriesActions): ProductsState{
+    switch (action.type) {
+        case ProductEntriesActionTypes.PRODUCT_ENTRY_CREATED:
+        case ProductEntriesActionTypes.PRODUCT_ENTRIES_LOADED:
+        case ProductEntriesActionTypes.PRODUCT_MAIN_ENTRY_SET:
+        case ProductEntriesActionTypes.PRODUCT_ENTRY_DELETED:
+        case ProductEntriesActionTypes.PRODUCT_ENTRY_UPDATED:
+            const id:string = action.id;
+            const product:IProduct = action.payload;
+            state = adapter.upsertOne(product,state);
+            if(state.selectedProduct._id === id){
+                state = {...state,selectedProduct:product}
+            }
+            return state;
         default:
             return state;
     }
@@ -46,3 +65,4 @@ export const selectAllProducts = createSelector(selectProductsState,selectAll);
 export const selectProductsEntities = createSelector(selectProductsState,selectEntities);
 export const selectProductsTotal = createSelector(selectProductsState,selectTotal);
 export const selectedProduct = createSelector(selectProductsState,(state)=>state.selectedProduct);
+export const selectProductByCodebar = createSelector(selectAllProducts,(products:IProduct[],props)=>products.find((entry)=>entry.codebar===props.codebar));
