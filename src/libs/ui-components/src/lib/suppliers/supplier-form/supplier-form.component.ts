aace03956ded8f836e6566d01dfc3423465829ef
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-06-28 18:48:38 +0100

    Refactor Supplier form component

--- src/libs/ui-components/src/lib/suppliers/supplier-form/supplier-form.component.ts
+++ src/libs/ui-components/src/lib/suppliers/supplier-form/supplier-form.component.ts
@@ -1,45 +1,53 @@
 import { ISupplier } from '@workspace/interfaces';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-
+export class SupplierForm {
+  form:FormGroup;
+  constructor(private fb:FormBuilder){}
+  protected buildForm(value){
+    const form:any = {
+      name:[""],
+      contact:this.fb.group({
+        email:[""],
+        firstName:[""],
+        lastName:[""],
+        phoneNumber:[""]
+      }),
+      address:this.fb.group({
+        address:[""],
+        city:[""],
+        zip:[""],
+      }),
+    }
+    if(value && value._id){
+      form._id = [""];
+    }
+    this.form=this.fb.group(form);
+  }
+  get value(){
+    return this.form.value;
+  }
+}
 @Component({
   selector: 'workspace-supplier-form',
   templateUrl: './supplier-form.component.html',
   styleUrls: ['./supplier-form.component.scss']
 })
-export class SupplierFormComponent implements OnInit {
-  form:FormGroup =this.fb.group({
-    name:[""],
-    contact:this.fb.group({
-      email:[""],
-      firstName:[""],
-      lastName:[""],
-      phoneNumber:[""]
-    }),
-    address:this.fb.group({
-      address:[""],
-      city:[""],
-      zip:[""],
-    }),
-  });
+export class SupplierFormComponent extends SupplierForm implements OnInit {
+  
   @Input()
   set supplier(value:ISupplier){
-    if(value._id){
-      this.form.addControl("_id",this.fb.control(""));
-    } 
+    this.buildForm(value);
+    if(value)
     this.form.patchValue(value);
   }
   @Output()
   formSubmit:EventEmitter<ISupplier> = new EventEmitter();
   constructor(
-    private fb:FormBuilder,
-  ) { }
+    fb:FormBuilder,
+  ) { super(fb)}
 
   ngOnInit(): void {
-  }
-
-  onSubmit(){
-    this.formSubmit.emit(this.form.value);
   }
 
 }
