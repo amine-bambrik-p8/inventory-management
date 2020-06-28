2e0d773f41c96cf3251e444abb43092672959248
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-06-28 16:20:02 +0100

    Extract category form to a separate class

--- src/libs/ui-components/src/lib/category/category-form/category-form.component.ts
+++ src/libs/ui-components/src/lib/category/category-form/category-form.component.ts
@@ -1,29 +1,39 @@
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit, Inject } from '@angular/core';
 import { ICategory } from '@workspace/interfaces';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+export class CategoryForm{
+  form:FormGroup;
+  constructor(private fb:FormBuilder){}
+  protected buildForm(value:ICategory){
+    const form:any = {
+      name:[""],
+    };
+    if(value && value._id){
+      form._id = [""];
+    }
+    this.form =  this.fb.group(form);
+  }
+}
 @Component({
   selector: 'workspace-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.scss']
 })
-export class CategoryFormComponent implements OnInit {
-  form = this.fb.group({
-    name:[""],
-  })
+export class CategoryFormComponent extends CategoryForm implements OnInit {
   constructor(
+    fb:FormBuilder,
     public dialogRef: MatDialogRef<CategoryFormComponent>,
-    @Inject(MAT_DIALOG_DATA) category: ICategory,
-    private fb:FormBuilder
+     @Inject(MAT_DIALOG_DATA) private initialValue: ICategory,
   ) {
-    this.form.patchValue(category);
+    super(fb);
+
   }
 
   ngOnInit(): void {
-  }
-  onSubmit(){
-
+    this.buildForm(this.initialValue);
+    if(this.initialValue)
+      this.form.patchValue(this.initialValue);
   }
   onClose(): void {
     this.dialogRef.close();
