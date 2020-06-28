bc4c9c5e1a6b52399c489a27f00020877beb6439
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-06-28 16:17:27 +0100

    Update Cart table UI and move handling event to cart component

--- src/libs/ui-components/src/lib/cart/cart-table/cart-table.component.ts
+++ src/libs/ui-components/src/lib/cart/cart-table/cart-table.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IOrderEntry } from '@workspace/interfaces';
-
+import { Component, OnInit, Input, EventEmitter, Output, ContentChild } from '@angular/core';
+import { ICartItem } from '@workspace/interfaces';
+import { CartFacade } from '@workspace/core-data'
 @Component({
   selector: 'workspace-cart-table',
   templateUrl: './cart-table.component.html',
@@ -8,13 +8,32 @@ import { IOrderEntry } from '@workspace/interfaces';
 })
 export class CartTableComponent implements OnInit {
   @Input()
-  entries:IOrderEntry[];
-  displayedColumns: string[] = ['product', 'quantity', 'unit', 'unit-price','discount','sub-total','action'];
-
-  dataSource = [];
-  constructor() { }
-
+  entries:ICartItem[];
+  @Input()
+  set actions(value:boolean){
+    if(value){
+      this.displayedColumns.push("action")
+    }
+    this.editable = value;
+  }
+  
+  @Output()
+  codebarEntered:EventEmitter<string> = new EventEmitter<string>();
+  @Output()
+  search:EventEmitter<string> = new EventEmitter();
+  
+  displayedColumns: string[] = ['product', 'quantity', 'unit', 'unit-price','discount','sub-total'];
+  editable:boolean;
+  constructor(private cartFacade:CartFacade) { }
   ngOnInit(): void {
   }
-
+  onEdit(item:ICartItem): void{
+    this.cartFacade.setSelectedOrderEntry(item._id);
+  }
+  onDelete(item:ICartItem): void{
+    this.cartFacade.removeProductFromCart(item._id);
+  }
+  get total():number{
+    return this.entries.reduce((sum,curr)=>sum+curr.price*(1-curr.discount)*curr.quantity,0) || 0;
+  }
 }
