17abc4f5593e634b55e79629e0376b5df26364ac
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-06-28 18:29:31 +0100

    Update product entry UI

--- src/libs/ui-components/src/lib/products/product-entry/product-entry.component.ts
+++ src/libs/ui-components/src/lib/products/product-entry/product-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IProductEntry } from '@workspace/interfaces';
 
 @Component({
   selector: 'workspace-product-entry',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-entry.component.scss']
 })
 export class ProductEntryComponent implements OnInit {
-
+  @Input()
+  entry:IProductEntry;
+  @Input()
+  highlight:boolean;
+  @Input()
+  editable:boolean
+  @Output()
+  edit:EventEmitter<IProductEntry> = new EventEmitter();
+  @Output()
+  delete:EventEmitter<IProductEntry> = new EventEmitter();
+  @Output()
+  entryHightlight:EventEmitter<IProductEntry> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
