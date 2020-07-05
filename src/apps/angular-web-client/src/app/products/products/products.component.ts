f930787b4bb13e2ef6ee44a396a043998d86a0b0
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-05 19:53:09 +0100

    Refactor Products component

--- src/apps/angular-web-client/src/app/products/products/products.component.ts
+++ src/apps/angular-web-client/src/app/products/products/products.component.ts
@@ -1,29 +1,37 @@
-import { ICategory, IProduct } from '@workspace/interfaces';
+import { IProduct } from '@workspace/interfaces';
 import { Component, OnInit } from '@angular/core';
-import { CategoriesFacade, ProductsFacade } from '@workspace/core-data';
-import { of, Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { CategoriesFacade, ProductsFacade, CartFacade } from '@workspace/core-data';
+import { Observable } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
-import { CategoryFormComponent } from '@workspace/ui-components';
-
+export enum LoadMode{
+  VIEW="VIEW",
+  SELECT="SELECT",
+}
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  categories$:Observable<ICategory[]> = this.categories.allCategories$;
   products$:Observable<IProduct[]> = this.products.allProducts$;
-  constructor(private categories:CategoriesFacade,private products:ProductsFacade,private router:Router,public dialog: MatDialog) { }
+  mode:LoadMode = LoadMode.VIEW;
+  constructor(
+    private categories:CategoriesFacade,
+    private products:ProductsFacade,
+    private router:Router,
+    public dialog: MatDialog,
+    private activatedRoute:ActivatedRoute,
+    private cartFacade:CartFacade
+    ) { }
 
   ngOnInit(): void {
+    this.mode = this.activatedRoute.snapshot.queryParamMap.get("mode") as LoadMode;
     this.loadData();
   }
   async loadData() {
     try {
-      await this.categories.loadCategories();
       await this.products.loadProducts();
-      
     } catch (error) {
       console.error("Failed to Load Data");
     }
@@ -32,23 +40,12 @@ export class ProductsComponent implements OnInit {
   onAdd(){
     this.router.navigate(["products","add"]);
   }
-  onAddCategory(){
-    const dialogRef = this.dialog.open(CategoryFormComponent, {
-      data: {name:""}
-    });
-
-    dialogRef.afterClosed().subscribe(async (category:ICategory) => {
-      if(category){
-        try {
-          await this.categories.addCategory(category);
-        } catch (error) {
-          console.error("Failed to create Category");
-          console.error(error);
-        }
-      }
-    });
-  }
-  onView(product:IProduct){
+  
+  onSelect(product:IProduct){
+    if(this.mode === LoadMode.SELECT){
+      this.cartFacade.addProductToCart(product);
+      return this.router.navigate(["checkout"])
+    }
     this.router.navigate(["products",product._id]);
   }
 
